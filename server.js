50513c32a02abf6db3c4ec54a194f191c0cedc30
mylenezijlstra <mylenezijlstra@icloud.com>
2026-06-22 11:44:38 +0200

    online zetten

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const http = require('http');
 const path = require('path');
 const cors = require('cors');
 const fetch = require('node-fetch');
-const { Server } = require('socket.io');
+// const { Server } = require('socket.io');
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
